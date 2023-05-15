1531fd953a47d89fe1157c7640bb34f7066510ba
ALUMNOS <ALUMNOS@P618>
2023-05-15 09:13:13 +0200

    yowy

---
+++ Examenmarcas/src/Examen/Examen_16_PRIMO.java
@@ -0,0 +1,10 @@
+package Examen;
+
+public class Examen_16_PRIMO {
+
+	public static void main(String[]args) {
+		System.out.println("Buenos días Ceinmark, soy Marta Gómez");
+		System.out.println("Introduce un número para averiguar si es primo o no: ");
+	}
+	
+}
